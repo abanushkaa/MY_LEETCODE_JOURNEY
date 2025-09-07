8ac290ca9a10f23f0db615efa332e6ee253eed93
Anushka Baranwal <141651897+abanushkaa@users.noreply.github.com>
2025-09-07 19:01:14 +0530

    Time: 6 ms (46.62%), Space: 69.4 MB (7.57%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // 1) Find the end of the first half
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+
+        // 2) Reverse the second half
+        ListNode secondHalfStart = reverse(firstHalfEnd.next);
+
+        // 3) Compare both halves
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+                break;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        // 4) Restore the list (optional, but good practice)
+        firstHalfEnd.next = reverse(secondHalfStart);
+
+        return result;
+    }
+
+    // Helper: Find the end of first half (left middle for even length)
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Helper: Reverse a linked list
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
